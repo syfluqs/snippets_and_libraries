5420237f33642cadb3ba75152502b4e128d1d83a
syfluqs <syfluqs@gmail.com>
2016-12-30 16:43:10 +0530

    find in vector

---
+++ comp_prog/find_in_vector.cpp
@@ -0,0 +1,9 @@
+td::find(vector.begin(), vector.end(), item) != vector.end()
+    This returns a bool (true if present, false otherwise). With your example:
+
+#include <algorithm>
+
+    if ( std::find(vector.begin(), vector.end(), item) != vector.end()  )
+       do_this();
+       else
+          do that();
